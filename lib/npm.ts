c3acd2f49819ded2f4a3d57b7c5a7599392f3ae1
Dan Schulte <daschult@microsoft.com>
2018-12-11 10:48:11 -0800

    Rework npm module some more

--- lib/npm.ts
+++ lib/npm.ts
@@ -1,19 +1,55 @@
 import * as os from "os";
 import { RunOptions, RunResult, runSync } from "./run";
 
-export function npmRun(args: string | string[], options?: RunOptions): RunResult {
+/**
+ * Run a NPM command.
+ * @param args The arguments to the NPM command.
+ * @param options The optional arguments that can be added to the NPM command.
+ * @returns The result of running the NPM command.
+ */
+export function npm(args: string | string[], options?: RunOptions): RunResult {
   const npmExecutable: string = (os.platform() === "win32" ? "npm.cmd" : "npm");
   return runSync(npmExecutable, args, options);
 }
+
+/**
+ * Run a script specified in the package.json file.
+ * @param args The arguments for the NPM run command.
+ * @param options The optional arguments that can be added to the NPM command.
+ * @returns The result of running the NPM command.
+ */
+export function npmRun(args: string | string[], options?: RunOptions): RunResult {
+  if (typeof args === "string") {
+    args = "run " + args;
+  } else {
+    args.unshift("run");
+  }
+  return npm(args, options);
+}
+
 
 /**
  * Run "npm install" from the optional packageFolderPath, or if packageFolderPath isn't specified,
  * then run "npm install" from the current directory.
  */
 export function npmInstall(options?: RunOptions): RunResult {
-  return npmRun("install", options);
+  return npm("install", options);
 }
 
+/**
+ * The optional arguments that can be passed to npmView().
+ */
+export interface NPMViewOptions extends RunOptions {
+  /**
+   * The name of the package to get details for. If this is undefined or empty, the package in the
+   * executionFolderPath will be used instead.
+   */
+  packageName?: string;
+}
+
+/**
+ * The result of running a npm view command.
+ */
 export interface NPMViewResult extends RunResult {
   _id?: string;
   _rev?: string;
@@ -60,8 +96,13 @@ export interface NPMViewResult extends RunResult {
   };
 }
 
-export function npmView(options?: RunOptions): NPMViewResult {
-  const commandResult: RunResult = npmRun(["view", "--json"], options);
+export function npmView(options?: NPMViewOptions): NPMViewResult {
+  const args: string[] = ["view"];
+  if (options && options.packageName) {
+    args.push(options.packageName);
+  }
+  args.push("--json");
+  const commandResult: RunResult = npm(args, options);
   const npmViewResponse: any = JSON.parse(commandResult.stdout.trim());
   return {
     commandResult,
