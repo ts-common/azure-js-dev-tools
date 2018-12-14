7d1b4ac323394a57407c8f8d5aa3ce19a02fd709
Dan Schulte <daschult@microsoft.com>
2018-12-14 14:17:06 -0800

    Add ##[error] prefix to error lines

--- lib/checkPackageJsonVersion.ts
+++ lib/checkPackageJsonVersion.ts
@@ -5,13 +5,13 @@ function checkPackageJsonVersion(startPath: string): number {
   console.log(`Looking for package.json file starting at "${startPath}"...`);
   const packageJsonFilePath: string | undefined = findPackageJsonFileSync(startPath);
   if (!packageJsonFilePath) {
-    console.error(`Could not find a package.json file at "${startPath}" or in any of its parent folders.`);
+    console.error(`##[error]Could not find a package.json file at "${startPath}" or in any of its parent folders.`);
     exitCode = 1;
   } else {
     console.log(`Found a package.json file at "${packageJsonFilePath}".`);
     const packageJson: PackageJson = readPackageJsonFileSync(packageJsonFilePath);
     if (isPackageJsonPublished(packageJson)) {
-      console.error(`A package with the name "${packageJson.name}" and the version "${packageJson.version}" already exists in NPM.`);
+      console.error(`##[error]A package with the name "${packageJson.name}" and the version "${packageJson.version}" already exists in NPM.`);
       exitCode = 2;
     } else {
       console.log(`No package exists yet with the name "${packageJson.name}" and the version "${packageJson.version}".`);
