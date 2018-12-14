9cfce377601fb469433b3440bc2f0fec015097fd
Dan Schulte <daschult@microsoft.com>
2018-12-14 13:47:19 -0800

    Try warning instead of error

--- lib/checkPackageJsonVersion.ts
+++ lib/checkPackageJsonVersion.ts
@@ -5,12 +5,12 @@ function checkPackageJsonVersion(startPath: string): boolean {
   console.log(`Looking for package.json file starting at "${startPath}"...`);
   const packageJsonFilePath: string | undefined = findPackageJsonFileSync(startPath);
   if (!packageJsonFilePath) {
-    throw new Error(`Could not find a package.json file at "${startPath}" or in any of its parent folders.`);
+    console.error(`Could not find a package.json file at "${startPath}" or in any of its parent folders.`);
   } else {
     console.log(`Found a package.json file at "${packageJsonFilePath}".`);
     const packageJson: PackageJson = readPackageJsonFileSync(packageJsonFilePath);
     if (isPackageJsonPublished(packageJson)) {
-      throw new Error(`A package with the name "${packageJson.name}" and the version "${packageJson.version}" already exists in NPM.`);
+      console.warn(`A package with the name "${packageJson.name}" and the version "${packageJson.version}" already exists in NPM.`);
     } else {
       console.log(`No package exists yet with the name "${packageJson.name}" and the version "${packageJson.version}".`);
       result = true;
