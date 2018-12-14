46e0613c808bd35876b1dc7d6580c6e5ffba03a6
Dan Schulte <daschult@microsoft.com>
2018-12-14 13:55:21 -0800

    Change back to exit codes

--- lib/checkPackageJsonVersion.ts
+++ lib/checkPackageJsonVersion.ts
@@ -1,22 +1,24 @@
 import { findPackageJsonFileSync, isPackageJsonPublished, PackageJson, readPackageJsonFileSync } from "./packageJson";
 
-function checkPackageJsonVersion(startPath: string): boolean {
-  let result = false;
+function checkPackageJsonVersion(startPath: string): number {
+  let exitCode: number;
   console.log(`Looking for package.json file starting at "${startPath}"...`);
   const packageJsonFilePath: string | undefined = findPackageJsonFileSync(startPath);
   if (!packageJsonFilePath) {
     console.error(`Could not find a package.json file at "${startPath}" or in any of its parent folders.`);
+    exitCode = 1;
   } else {
     console.log(`Found a package.json file at "${packageJsonFilePath}".`);
     const packageJson: PackageJson = readPackageJsonFileSync(packageJsonFilePath);
     if (isPackageJsonPublished(packageJson)) {
-      console.warn(`A package with the name "${packageJson.name}" and the version "${packageJson.version}" already exists in NPM.`);
+      console.error(`A package with the name "${packageJson.name}" and the version "${packageJson.version}" already exists in NPM.`);
+      exitCode = 2;
     } else {
       console.log(`No package exists yet with the name "${packageJson.name}" and the version "${packageJson.version}".`);
-      result = true;
+      exitCode = 0;
     }
   }
-  return result;
+  return exitCode;
 }
 
-checkPackageJsonVersion(__dirname);
+process.exit(checkPackageJsonVersion(__dirname));
