c81564c3f44fd923909c18a0909ccb8e35f8550b
Dan Schulte <daschult@microsoft.com>
2018-12-14 13:44:50 -0800

    Change error logs to errors

--- lib/checkPackageJsonVersion.ts
+++ lib/checkPackageJsonVersion.ts
@@ -5,12 +5,12 @@ function checkPackageJsonVersion(startPath: string): boolean {
   console.log(`Looking for package.json file starting at "${startPath}"...`);
   const packageJsonFilePath: string | undefined = findPackageJsonFileSync(startPath);
   if (!packageJsonFilePath) {
-    console.error(`Could not find a package.json file at "${startPath}" or in any of its parent folders.`);
+    throw new Error(`Could not find a package.json file at "${startPath}" or in any of its parent folders.`);
   } else {
     console.log(`Found a package.json file at "${packageJsonFilePath}".`);
     const packageJson: PackageJson = readPackageJsonFileSync(packageJsonFilePath);
     if (isPackageJsonPublished(packageJson)) {
-      console.error(`A package with the name "${packageJson.name}" and the version "${packageJson.version}" already exists in NPM.`);
+      throw new Error(`A package with the name "${packageJson.name}" and the version "${packageJson.version}" already exists in NPM.`);
     } else {
       console.log(`No package exists yet with the name "${packageJson.name}" and the version "${packageJson.version}".`);
       result = true;
@@ -19,8 +19,4 @@ function checkPackageJsonVersion(startPath: string): boolean {
   return result;
 }
 
-function main(): void {
-  process.exit(checkPackageJsonVersion(__dirname) ? 0 : 1);
-}
-
-main();
+checkPackageJsonVersion(__dirname);
