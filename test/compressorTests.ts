1661084a364b943bdf10ca6c789a0cfa509a4b2a
Dan Schulte <daschult@microsoft.com>
2019-05-30 15:54:41 -0700

    Add Compressor.zipFiles()

--- test/compressorTests.ts
+++ test/compressorTests.ts
@@ -3,35 +3,71 @@ import { deleteFile, fileExists, joinPath, readFileContents } from "../lib";
 import { ArchiverCompressor, CompressionResult, FakeCompressor } from "../lib/compressor";
 
 describe("compressor.ts", function () {
-  it("FakeCompressor", async function () {
-    const compressor = new FakeCompressor();
-    const folderPath: string = __dirname;
-    const zipFilePath: string = joinPath(__dirname, "../test.zip");
-    const result: CompressionResult = await compressor.zip(folderPath, zipFilePath);
-    try {
-      assert.deepEqual(result, {
-        errors: [],
-        warnings: []
-      });
-      assert.strictEqual(await readFileContents(zipFilePath), `folder: ${folderPath}`);
-    } finally {
-      deleteFile(zipFilePath);
-    }
+  describe("FakeCompressor", function () {
+    it("zipFiles()", async function () {
+      const compressor = new FakeCompressor();
+      const filePath: string = __filename;
+      const zipFilePath: string = joinPath(__dirname, "../test.zip");
+      const result: CompressionResult = await compressor.zipFiles(filePath, zipFilePath);
+      try {
+        assert.deepEqual(result, {
+          errors: [],
+          warnings: []
+        });
+        assert.strictEqual(await readFileContents(zipFilePath), `files:\n${filePath}\n`);
+      } finally {
+        deleteFile(zipFilePath);
+      }
+    });
+
+    it("zipFolder()", async function () {
+      const compressor = new FakeCompressor();
+      const folderPath: string = __dirname;
+      const zipFilePath: string = joinPath(__dirname, "../test.zip");
+      const result: CompressionResult = await compressor.zipFolder(folderPath, zipFilePath);
+      try {
+        assert.deepEqual(result, {
+          errors: [],
+          warnings: []
+        });
+        assert.strictEqual(await readFileContents(zipFilePath), `folder: ${folderPath}`);
+      } finally {
+        deleteFile(zipFilePath);
+      }
+    });
   });
 
-  it("ArchiverCompressor", async function () {
-    const compressor = new ArchiverCompressor();
-    const folderPath: string = __dirname;
-    const zipFilePath: string = joinPath(__dirname, "../test.zip");
-    const result: CompressionResult = await compressor.zip(folderPath, zipFilePath);
-    try {
-      assert.deepEqual(result, {
-        errors: [],
-        warnings: []
-      });
-      assert.strictEqual(await fileExists(zipFilePath), true);
-    } finally {
-      deleteFile(zipFilePath);
-    }
+  describe("ArchiverCompressor", function () {
+    it("zipFiles()", async function () {
+      const compressor = new ArchiverCompressor();
+      const filePath: string = __filename;
+      const zipFilePath: string = joinPath(__dirname, "../test.zip");
+      const result: CompressionResult = await compressor.zipFiles(filePath, zipFilePath);
+      try {
+        assert.deepEqual(result, {
+          errors: [],
+          warnings: []
+        });
+        assert.strictEqual(await fileExists(zipFilePath), true);
+      } finally {
+        deleteFile(zipFilePath);
+      }
+    });
+
+    it("zipFolder()", async function () {
+      const compressor = new ArchiverCompressor();
+      const folderPath: string = __dirname;
+      const zipFilePath: string = joinPath(__dirname, "../test.zip");
+      const result: CompressionResult = await compressor.zipFolder(folderPath, zipFilePath);
+      try {
+        assert.deepEqual(result, {
+          errors: [],
+          warnings: []
+        });
+        assert.strictEqual(await fileExists(zipFilePath), true);
+      } finally {
+        deleteFile(zipFilePath);
+      }
+    });
   });
 });
